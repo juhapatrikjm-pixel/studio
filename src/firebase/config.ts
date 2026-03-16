33afc604cd19d25bbda771609d56caf8981fe83d
Juha Makkonen <juha.patrik.JM@gmail.com>
2026-03-16 07:06:24 +0000

    NEXT_PUBLIC_FIREBASE_API_KEY=AIzaSyB5il5BmI8-DRefraVg-5PqP1SECPBgzjY NEX

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -8,8 +8,9 @@
 export const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "wisemisa-d2b98",
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "wisemisa-d2b98.firebasestorage.app",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
